14fc5daddb8cc3352e2efd3897b120da7439569b
nandini8 <nandini.soni8@gmail.com>
2019-02-25 20:15:25 +0530

    Changes in fieWriter file

--- Private Nodes/fileWriter.js
+++ Private Nodes/fileWriter.js
@@ -1,5 +1,6 @@
-const fs = require('fs').promisees;
-const folder = require('hidefile');
+const fs = require('fs.promises');
+const mkdirp = require('mkdirp');
+
 
 function createFolder(){
   let os_type = process.platform;
@@ -8,105 +9,92 @@ function createFolder(){
   if(os_type == 'linux'){
       let dir = './.usernames';
       let dir2 = './.passwords';
-
-      if (!fs.existsSync(dir)){
-          fs.mkdirSync(dir);
-        }
-        if (!fs.existsSync(dir2)){
-            fs.mkdirSync(dir2);
-          }
-      }
+      mkdirp(dir, function(err) {
+        console.log(err);
+      });
+      mkdirp(dir2, function(err) {
+        console.log(err);
+      });
+    }
 
   //TO BE CHECKED
-  else{
-    if(os_type == 'win32'){
-      var fsWin=require('fswin');
-      var pathToFileOrDir='d:\\usernames\\';
-      var attributes={
-        	IS_ARCHIVED:false,//true means yes
-        	IS_HIDDEN:true,//false means no
-        	//IS_NOT_CONTENT_INDEXED:true,//remove this attribute if you don't want to change it
-        	IS_OFFLINE:false,
-        	IS_READ_ONLY:true,
-        	IS_SYSTEM:false,
-        	IS_TEMPORARY:false
-        };
-    }
-  }
+  // else{
+  //   if(os_type == 'win32'){
+  //     var fsWin=require('fswin');
+  //     var pathToFileOrDir='d:\\usernames\\';
+  //     var attributes={
+  //       	IS_ARCHIVED:false,//true means yes
+  //       	IS_HIDDEN:true,//false means no
+  //       	//IS_NOT_CONTENT_INDEXED:true,//remove this attribute if you don't want to change it
+  //       	IS_OFFLINE:false,
+  //       	IS_READ_ONLY:true,
+  //       	IS_SYSTEM:false,
+  //       	IS_TEMPORARY:false
+  //       };
+  //   }
+  // }
+}
+function random(low, high) {
+  return Math.random() * (high - low) + low
 }
 
-function writer_username(domain, username, password){
+async function writer_username(domain, username, password){
     //domain.file1
     //Username :  ID
 
     let user_key = 0;
+    let keys = [];
     createFolder();
-    fname = './.usernames/' + domain.concat(".username"); //change 'username'
-    fs.exists(fname, (exist) => {
-    	if(exist){
-	        fs.readFile(fname, 'utf8', function readFileCallback(err, data){
-    	        if (err) throw err;
-    	        obj = JSON.parse(data); //now it an object
-              let keys = [];
-              for(x in obj) {console.log(x) ; keys.push(x);}
-              obj[username] = keys.length + 1; //add some data
-              user_key = obj[username];
-    	        json = JSON.stringify(obj); //convert it back to json
-    	        fs.writeFile(fname, json, 'utf8', (err) => {
-		            if (err) throw err;
-	            }); // write it back
-            });
-        }
-        else{
-            obj = {};
-            obj[username] = 1; //add some data
-            user_key = obj[username];
-      	    json = JSON.stringify(obj); //convert it back to json
-      	    fs.writeFile(fname, json, 'utf8', (err) => {
-  		        if (err) throw err;
-	        }); // write it back
-        }
-    });
+    let fname = './.usernames/' + domain.concat(".username"); //change 'username'
 
+    let exists = await fs.exists(fname);
+
+    if(exists) {
+        let data = await fs.readFile(fname, 'utf8');
+
+        obj = JSON.parse(data); //now it an object
+        for(x in obj) {console.log(obj[x]) ; keys.push(obj[x]);}
+        obj[username] = random(0,10); //add some data
+        user_key = obj[username];
+        json = JSON.stringify(obj); //convert it back to json
+
+        await fs.writeFile(fname, json, 'utf8');
+    } else{
+          obj = {};
+          obj[username] = random(0,10); //add some data
+          user_key = obj[username];
+          json = JSON.stringify(obj); //convert it back to json
+
+          await fs.writeFile(fname, json, 'utf8');
+    }
 
 
     //ID : Password
     //domain.file2
     console.log("user_key value after changing" + user_key)
+
+
     let fname2 = './.passwords/' + domain.concat(".password");
-    fs.exists(fname2, (exist) => {
-    	if(exist){
-	        fs.readFile(fname2, 'utf8', function readFileCallback(err, data){
-    	        if (err) throw err;
-    	        obj = JSON.parse(data); //now it an object
-                let keys = [];
-                for(x in obj) keys.push(x);
-                if(user_key in keys)
-                {
-                  obj[user_key] = password;
-                }
-                else{
-                obj[keys.length + 1] = password; //add some data
-                }
-    	        json = JSON.stringify(obj); //convert it back to json
-    	        fs.writeFile(fname2, json, 'utf8', (err) => {
-		            if (err) throw err;
-	            }); // write it back
-            });
-        }
-        else{
-            obj = {};
-            obj[1] = password; //add some data
-    	    json = JSON.stringify(obj); //convert it back to json
-    	    fs.writeFile(fname2, json, 'utf8', (err) => {
-		        if (err) throw err;
-	        }); // write it back
-        }
-    });
+    console.log(fname2);
+    exists = await fs.exists(fname2);
+    console.log(exists);
+    if(exists) {
+      data = await fs.readFile(fname2, 'utf8');
+      obj = JSON.parse(data); //now it an object
+      console.log("dgs" + user_key);
+      if(user_key in keys){
+        console.log("  qwertyui"+keys)
+      }
+      obj[user_key] = password;
+      json = JSON.stringify(obj); //convert it back to json
+      await fs.writeFile(fname2, json, 'utf8');
+    } else{
+          obj = {};
+          obj[user_key] = password; //add some data
+        json = JSON.stringify(obj); //convert it back to json
+        await fs.writeFile(fname2, json, 'utf8');
+      }
 }
 
 module.exports = { writer_username };
 writer_username('facebook', 'nandini', 'password');
-// writer_username('facebook', 'nandini_username', 'password2');
-// writer_username('gmail', 'nandini', 'password');
-// writer_username('gmail', 'nandini_gmail', 'password');
